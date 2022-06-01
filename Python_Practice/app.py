efbf99024c1dc191c4b1cef90a8afeeb00e36a9a
MehmetMSezgin <mehmetmsezgin@gmail.com>
2022-06-01 16:08:59 +0300

    python practice day1

---
+++ Python_Practice/app.py
@@ -0,0 +1,63 @@
+from math import *
+
+print("Hello World")
+print("6/1/2022")
+
+print("  /|")
+print(" / |")
+print("/  |")
+
+#variables
+character_name = "Muki"
+character_age = "31"
+
+print("name " + character_name)
+print("age " + character_age)
+#int and str no concat in python
+
+character_name = "Mike"
+print("name " + character_name)
+
+character_age = 31.5
+is_Male = True
+print(character_age)
+
+print("M\nM\nS") #next line
+shortName = "MMS"
+print(shortName + "---")
+
+lowerCase= "MAKE IT LOWER CASE"
+print(lowerCase.lower())
+
+upperCase= "upper case"
+print(upperCase.upper())
+print(upperCase.isupper())
+print(upperCase.upper().isupper())
+
+length= "123456"
+print(len(length))
+
+print(shortName[0])
+print(shortName[1])
+print(shortName[2])
+
+school = "Coding Academy"
+print(school.index("A"))
+print(school.index("a"))
+print(school.replace("Coding", "Testing"))
+
+#numbers
+print(-2 / 5.5)
+print(10 % 3)
+my_num = 5
+print("my number is " + str(my_num))    #convert number to string
+
+no=-5
+print(abs(no))        #absolute value
+print(pow(3, 2))        #power 3^2
+print(max(89, 1))      #get the max
+print(min(89, 1))
+print(round(3.2))       #3 -- if you want to get 3 no matter what use floor
+print(round(3.7))       #4 -- if you want to get 4 no matter what use ceil
+print(sqrt(36))
+
---
+++ Python_Practice/getInputFromUser.py
@@ -0,0 +1,9 @@
+userName = input("Enter your name: ")
+surname = input(" Surname?: ")
+print("Hello " + userName + "surname " + surname)
+
+#basic calculator
+number1 = input("enter number boss: ")    #python gets as string even if you put numbert
+number2 = input("enter second number boss: ")
+result = float(number1) + float(number2)    #convert to integer or float
+print(result)
