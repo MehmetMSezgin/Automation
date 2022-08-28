b76c0c4e026db3e20923d150bc94119a993c6176
MehmetMSezgin <mehmetmsezgin@gmail.com>
2022-08-28 13:48:14 +0300

    almost done vala

--- Python_Practice/ReadingFiles.py
+++ Python_Practice/ReadingFiles.py
@@ -4,14 +4,14 @@
 # "r+" read and write
 
 example_file = open("Example.txt", "r")
-#lets check it is readable or not
+# lets check it is readable or not
 print(example_file.readable())
-#it is true so lets read
+# it is true so lets read
 print(example_file.read())
-#read lines
+# read lines
 print(example_file.readline())
 print(example_file.readline())
-#do not forget to close
+# do not forget to close
 example_file.close()
 
 #############################
@@ -26,12 +26,12 @@ print(office_employees.readlines())
 office_employees.close()
 
 ##############################
-#appending file
+# appending file
 emp = open("office.txt", "a")
 emp.write("\nToby - Hr")
 emp.close()
 ###########################
-#writing a new file
+# writing a new file
 create_txt_file = open("createdTxtFile.txt", "w")
 create_txt_file.write("this file has been created by using python codes")
 create_txt_file.close()
--- VALA/Multiples.py
+++ VALA/Multiples.py
@@ -1,20 +1,74 @@
+# final solution
+final_answer = []
+
 # read numbers
 number_sets = open("input.txt", "r")
+
 list_of_rows = (number_sets.readlines())
-print(list_of_rows)
-print(len(list_of_rows))
-print(list_of_rows[0])
-#her boslukta sayiyi cek burdan
-'''list = number_sets.readline()
-print(len(list))
-print(list)
+# print(list_of_rows)
+# ['5 8 31\n', '4 7 20']
 
-x = [100,2,300,4,75]
-dct = {}
-for i in x:
-    dct['lst_%s' % i] = []
+number_of_rows = len(list_of_rows)
+# print(len(list_of_rows))
+# 2
 
-print(dct)
-# {'lst_300': [], 'lst_75': [], 'lst_100': [], 'lst_2': [], 'lst_4': []}
 
-'''
+for j in range(number_of_rows):
+    # print(list_of_rows[0])  # 5 8 31
+    string = list_of_rows[j]
+    x = 1
+    space_count = 0
+    A = ""
+    B = ""
+    M = ""
+    answer_row = []
+    for i in range(len(string)):
+        letter = string[i:x]
+
+        if space_count == 0:
+            if A != "":
+                A = A + letter
+            else:
+                A = letter
+            if letter == " ":
+                space_count = space_count + 1
+        elif space_count == 1:
+            if B != "":
+                B = B + letter
+            else:
+                B = letter
+            if letter == " ":
+                space_count = space_count + 1
+        elif letter != " " and space_count == 2:
+            if M != "":
+                M = M + letter
+            else:
+                M = letter
+        x = x + 1
+    M = M.strip()
+    final_answer.append(M + ":")
+    A = int(A)
+    B = int(B)
+    M = int(M)
+
+    for x in range(1, M):
+        if x == A or x == B:
+            answer_row.append(x)
+        elif x % A == 0 or x % B == 0:
+            answer_row.append(x)
+    print(answer_row)
+    final_answer = final_answer + answer_row
+    final_answer.append("\n")
+
+print(final_answer)
+print("***********************")
+number_sets.close()
+
+final_answer_string = ','.join(str(x) for x in final_answer)
+
+open("output.txt", "w")
+output = open("output.txt", "a")
+output.write(final_answer_string)
+output.close()
+
+
