bc71b177c416e563894ff28a8e36fb0f14ed5229
MehmetMSezgin <mehmetmsezgin@gmail.com>
2023-03-22 17:05:47 +0200

    Day6 Functions while loop

---
+++ Python_100Days_Challenge/Day6_Functions.py
@@ -0,0 +1,18 @@
+def while_loop():
+    i = 0
+    while i < 5:
+        print(i)
+        i += 1
+
+
+while_loop()
+
+
+def while_loop2():
+    i = 0
+    while i != 10:
+        print("<--*-*-->")
+        i += 1
+
+
+while_loop2()
