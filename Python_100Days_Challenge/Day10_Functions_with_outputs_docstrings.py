37e1ff252a6722d003cb95f56d8234499a5e5e63
MehmetMSezgin <mehmetmsezgin@gmail.com>
2023-04-28 16:44:58 +0300

    Day 10

---
+++ Python_100Days_Challenge/Day10_Functions_with_outputs_docstrings.py
@@ -0,0 +1,6 @@
+def return_my_name(name):
+    return f"your name is {name}"
+
+
+variable = return_my_name("DeAngelo")
+print(variable)
--- Python_100Days_Challenge/Day9_Dictionary.py
+++ Python_100Days_Challenge/Day9_Dictionary.py
@@ -1,31 +1,69 @@
-my_dictionary = {"key_1": "def_1",
-                 "key_2": "def_2",
-                 123: "numbers"
+def dictionary_intro():
+    my_dictionary = {"key_1": "def_1",
+                     "key_2": "def_2",
+                     123: "numbers"
+                     }
+
+    print(my_dictionary["key_1"])
+
+    empty_dic = {}
+
+    # adding item
+    empty_dic["new_item"] = "New item is added"
+    print(empty_dic)
+
+    # wiping data inside
+    empty_dic = {}
+    print(empty_dic)
+
+    # editing
+    my_dictionary["key_1"] = "def_1 modified"
+    print(my_dictionary)
+
+    # loop throught in a dict
+    dictionary = {
+        "x": "letter1",
+        "y": "letter2",
+        "z": "letter3"
+    }
+
+    for item in dictionary:
+        print(item)
+        # prints only keys
+
+
+def nested_dictionary():
+    capitals = {"France": "Paris", "Germany": "Berlin"}
+    travel_log = {"Germany": ["Berlin", "Koln"],
+                  "Italy": ["Milano", "Venize"]
+                  }
+    # key is string , value is list here
+
+    countries = {"USA": {"states": ["Texas", "California", "Florida"],
+                         "visited_states": 3,
+                         "not_visited": 47}
                  }
+    # nested dict
 
-print(my_dictionary["key_1"])
+    my_list = [
+        {
+            "country": "France",
+            "cities_visited": ["Paris", "Dijon"],
+            "how_many_times": 2
+        },
+        {
+            "country": "Finland",
+            "cities_visited": ["Hsl", "Tur"],
+            "how_many_times": 3
+        }
+    ]
 
-empty_dic = {}
+    # nested dict in list
 
-# adding item
-empty_dic["new_item"] = "New item is added"
-print(empty_dic)
+    # add new country to list
+    my_list.append({"country": "GB", "countries_visited": ["London"], "how_many_times": 1})
+    print(my_list)
+    print(my_list[0]["cities_visited"][1])
+    my_list = []
 
-# wiping data inside
-empty_dic = {}
-print(empty_dic)
-
-# editing
-my_dictionary["key_1"] = "def_1 modified"
-print(my_dictionary)
-
-# loop throught in a dict
-dictionary = {
-    "x": "letter1",
-    "y": "letter2",
-    "z": "letter3"
-}
-
-for item in dictionary:
-    print(item)
-    # prints only keys
+nested_dictionary()
